98f0919a70fe30c72889940942de971fa79210a8
vanessabertoletti@gmail.com <vbertoletti@Vanessas-MacBook-Pro.local>
2018-07-17 15:10:11 -0700

    additional project details

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,49 @@
 //modules you need:
-var gulp = require('gulp');
+var gulp = require('gulp'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename'),
+  browserSync = require('browser-sync').create(),
+  eslint = require('gulp-eslint');
 
-//tasks below/what do you want gulp to do
-gulp.task('default', defaultTask);
 
-function defaultTask(done) {
-  // place code for your default task here
-  done();
-}
+  gulp.task("lint", function() {
+    return (gulp 
+      .src("./js/*.js")
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError() )
+        
+    );
+  });
+
+
+gulp.task('scripts', gulp.series("lint", function(){
+  return gulp
+    .src("./js/*.js")
+    .pipe( uglify() )
+    .pipe( rename({ extname: ".min.js" }) )
+    .pipe( gulp.dest( "./build/js"));
+})
+);
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+      server: {
+          baseDir: "./"
+      }
+  });
+
+gulp
+.watch("build/js/*.js")
+.on("change", browserSync.reload);
+
+});
+
+gulp.task("watch", function(){
+  gulp.watch("js/*.js", gulp.series("scripts"));
+
+});
+
+gulp.task("default", gulp.parallel("browser-sync", "watch"));
+
+
--- js/project.js
+++ js/project.js
@@ -13,6 +13,7 @@ $(document).ready(function() {
       throw err;
     });
 
+    console.jog("hello");
 
 
 });
