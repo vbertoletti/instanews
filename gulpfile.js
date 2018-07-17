f718367a2baa55f3ed0729734c0dce212f86418f
vanessabertoletti@gmail.com <vbertoletti@Vanessas-MacBook-Pro.local>
2018-07-17 10:09:55 -0700

    Adding gulp.

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+//modules you need:
+var gulp = require('gulp');
+
+//tasks below/what do you want gulp to do
+gulp.task('default', defaultTask);
+
+function defaultTask(done) {
+  // place code for your default task here
+  done();
+}
