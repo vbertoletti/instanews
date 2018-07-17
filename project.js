da521e7e187ce51010b191e3468ba13973c9a20b
vanessabertoletti@gmail.com <vbertoletti@Vanessas-MacBook-Pro.local>
2018-07-17 09:26:18 -0700

    First commit forinstanews website.

---
+++ project.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+    var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+    url += '?' + $.param({
+      'api-key': "ce812b641d764fcb9e91d0f15205783d"
+    });
+    $.ajax({
+      url: url,
+      method: 'GET',
+    }).done(function(result) {
+      console.log(result);
+    }).fail(function(err) {
+      throw err;
+    });
+
+
+
+});
